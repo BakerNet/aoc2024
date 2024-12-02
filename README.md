76ab7c7bbb6e930890184c8ca55c766530b564db
BakerNet <hansbaker90@gmail.com>
2024-12-01 22:25:41 -0800

    Day 2 in the books

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Code | Part 1 | Part 2 |
 | :---: | :---: | :---: | :---: |
 | [Day 1](https://adventofcode.com/2024/day/1) | [code](src/bin/01.rs) | ⭐ | ⭐ |
-| [Day 2](https://adventofcode.com/2024/day/2) | [code](src/bin/02.rs) | _ | _ |
+| [Day 2](https://adventofcode.com/2024/day/2) | [code](src/bin/02.rs) | ⭐ | ⭐ |
 | [Day 3](https://adventofcode.com/2024/day/3) | [code](src/bin/03.rs) | _ | _ |
 | [Day 4](https://adventofcode.com/2024/day/4) | [code](src/bin/04.rs) | _ | _ |
 | [Day 5](https://adventofcode.com/2024/day/5) | [code](src/bin/05.rs) | _ | _ |
---
+++ src/bin/02.rs
@@ -0,0 +1,79 @@
+use itertools::Itertools;
+
+advent_of_code::solution!(2);
+
+fn process_input(input: &str) -> Vec<Vec<u64>> {
+    input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|c| c.parse::<u64>().expect("Should all be numbers"))
+                .collect_vec()
+        })
+        .collect_vec()
+}
+
+fn is_increasing(v: &Vec<u64>) -> bool {
+    let one = v[0] < v[1];
+    let two = v[1] < v[2];
+    let three = v[2] < v[3];
+    one && two || two && three || one && three
+}
+
+fn is_bad(x: u64, y: u64, increasing: bool) -> bool {
+    let (x, y) = if increasing { (x, y) } else { (y, x) };
+    x == y || x > y || y - x > 3
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let lines = process_input(input);
+    Some(
+        lines
+            .into_iter()
+            .filter(|v| {
+                let increasing = is_increasing(v);
+                !v.windows(2).any(|w| is_bad(w[0], w[1], increasing))
+            })
+            .count() as u64,
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let lines = process_input(input);
+    Some(
+        lines
+            .into_iter()
+            .filter(|v| {
+                let up = is_increasing(v);
+                let bad = v.windows(2).find_position(|w| is_bad(w[0], w[1], up));
+                if let Some((i, _)) = bad {
+                    let mut nv = v.clone();
+                    nv.remove(i);
+                    let mut nv2 = v.clone();
+                    nv2.remove(i + 1);
+                    !nv.windows(2).any(|w| is_bad(w[0], w[1], up))
+                        || !nv2.windows(2).any(|w| is_bad(w[0], w[1], up))
+                } else {
+                    true
+                }
+            })
+            .count() as u64,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(4));
+    }
+}
